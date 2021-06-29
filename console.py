4ccbf72670599a085967b392bc8a4c1bbba96a22
ronLabs <ron.altamir@outlook.com>
2021-06-29 21:36:43 +0000

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" main program executable """
+
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """ command interpreter """
+
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """ override emptyline func """
+        pass
+
+    def do_quit(self, arg):
+        """ Close """
+        exit()
+
+    def do_EOF(self, arg):
+        """ close """
+        exit()
+
+    def help_EOF(self):
+        """ eof help """
+        print("Quit command to exit program: cntrl + C\n")
+
+    def do_create(self, arg):
+        
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
