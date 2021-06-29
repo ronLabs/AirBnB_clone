cc298eff6adb7b67f55ac517e22717831871cedd
ronLabs <ron.altamir@outlook.com>
2021-06-29 23:14:36 +0000

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,9 @@ class BaseModel():
                     setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
-            self.updated_at = datetime.now()
             self.created_at = datetime.now()
-
+            self.updated_at = datetime.now()
+            
     def __str__(self):
         """return format string"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
@@ -33,7 +33,7 @@ class BaseModel():
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
-        new_dict['__class__'] = self.__class__.__name__
         return new_dict
