5d19760084e28b33fc59868ec7380aeca655a756
Melisa Rojas <melisarv@gmail.com>
2021-06-30 03:33:49 -0500

    fix task nro 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,30 +7,16 @@ from datetime import datetime
 class BaseModel():
     """definition class BaseModel"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initializes method"""
-        tformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    setattr(self, k, v)
-            if 'id' in kwargs.keys():
-                self.id = kwargs['id']
-            if 'created_at' in kwargs.keys():
-                self.created_at = datetime.strptime(
-                                           kwargs['created_at'], tformat)
-            if 'updated_at' in kwargs.keys():
-                self.updated_at = datetime.strptime(
-                                           kwargs['updated_at'], tformat)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """return format string"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
         """updates the attribute updated_at"""
